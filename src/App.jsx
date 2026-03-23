0d506ce65cf5bc1813c1055a1e66782fcb79b822
Risu02 <geromedones@gmail.com>
2026-03-24 09:51:02 +1300

    buh

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+import { createContext, useState } from 'react'
 import NavMenu from './NavMenu.jsx'
 import ScpDetail from './ScpDetail.jsx'
 import Welcome from './Welcome.jsx'
 import './App.css'
 
+export const NavContext = createContext()
+
+function AppInner() {
+  const location = useLocation()
+  const onHome = location.pathname === '/'
+  const [navLocked, setNavLocked] = useState(true)
+
+  // If not on the home page, nav is always accessible
+  const isLocked = onHome ? navLocked : false
+
+  return (
+    <NavContext.Provider value={{ navLocked: isLocked, setNavLocked }}>
+      <NavMenu />
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        <Route path='/scp/:id' element={<ScpDetail />} />
+      </Routes>
+    </NavContext.Provider>
+  )
+}
+
 export default function App() {
   return (
     <div>
       <Router>
-        <NavMenu />
-        <Routes>
-          <Route path='/' element={<Welcome />} />
-          <Route path='/scp/:id' element={<ScpDetail />} />
-        </Routes>
+        <AppInner />
       </Router>
     </div>
   )
